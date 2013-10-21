2a9ff56cb26dd51b8a209604826df5dd6d93f4da
Peter D. Cassetta <peter@cassetta.info>
2013-10-21 18:37:36 -0500

    Reverting cm.mk to original

--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := fx3mt
+PRODUCT_RELEASE_NAME := fx3
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/fx3mt/device_fx3mt.mk)
+$(call inherit-product, device/lge/fx3/device_fx3.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := fx3mt
-PRODUCT_NAME := cm_fx3mt
-PRODUCT_BRAND := MetroPCS
-PRODUCT_MODEL := LG-MS659
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_DEVICE := fx3
+PRODUCT_NAME := cm_fx3
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := fx3
+PRODUCT_MANUFACTURER := lge
